ffa9edbe2e5e860ed9a80050515669c6545497dd
taichi-de <61309248+taichi-de@users.noreply.github.com>
2022-03-22 13:23:09 +0100

    adjusted

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,8 @@
 public class Game {
     public int currentTurn = 1;
     public int winPlayer = 0;
-    public String playerMove;
-    int skipCheck = 0;
+    public String playerSymbol;
+    int skipCheck = 0; // Error? -> 0 or 1
     int currentLine;
     int currentColumn;
 
@@ -22,47 +22,46 @@ public class Game {
 
     public String makeMove(int playerInput){
         if(playerInput < 8){
-            if(currentTurn == 1) playerMove = "X";
-            else if(currentTurn == 2) playerMove = "O";
-            else playerMove = "?";
-            for(int i = 0; i < 7; i++) {
-                try {
-                    if (gameField[i][playerInput - 1] == "*") { // 5
+
+            if(currentTurn == 1) playerSymbol = "X";
+            else playerSymbol = "O";
+
+            for(int i = 0; i < 7; i++){
+                try{
+                    if(gameField[i][playerInput - 1] == "*") {// ist der Platz noch frei?
                         continue;
-                    }
-                    else if (gameField[i][playerInput - 1] == "X" ||
-                        gameField[i][playerInput - 1] == "O"){
-                        gameField[i - 1][playerInput - 1] = playerMove; skipCheck = 0; break;
-                    }
-                    else {
-                        gameField[i - 1][playerInput - 1] = playerMove;
+                    }else if(gameField[i][playerInput - 1] == "X" || gameField[i][playerInput - 1] == "O"){// ist da schon ein Symbol? -> eine Zeile hoeher
+                        gameField[i - 1][playerInput - 1] = playerSymbol;
+                        skipCheck = 0;
+                        break;
+                    }else{
+                        gameField[i - 1][playerInput - 1] = playerSymbol;
                         skipCheck = 0;
                     }
-                } catch(ArrayIndexOutOfBoundsException e) {
+                }catch(ArrayIndexOutOfBoundsException e) {//eine Spalte ist voll?
                     skipCheck = 1;
-                    return "The column is overfilled!! ["+((currentTurn == 1) ?
-                        "X" : "O")+"] can have another turn!";
+                    return "The column is overfilled!! [" + ((currentTurn == 1) ? "X" : "O") + "] can have another turn!";
                 }
             }
             return "~~~~~~~~~~~~~~~~~~~~~~~~~";
-        } else {
+        }else{
             skipCheck = 1;
-            return "Input error. ["+playerMove+"] can have another turn!";
+            return "Input error. [" + playerSymbol + "] can have another turn!";
         }
     }
 
     public int checkGame (int playerID){
-        if(playerID == 1) playerMove = "X";
-        if(playerID == 2) playerMove = "O";
+        if(playerID == 1) playerSymbol = "X";
+        else playerSymbol = "O";
 
         // check the Field horizontally
         currentLine = 0;
         currentColumn = 0;
         while (currentColumn < 4) {
-            if (gameField[currentLine][currentColumn] == playerMove &&
-                gameField[currentLine][currentColumn + 1] == playerMove &&
-                gameField[currentLine][currentColumn + 2] == playerMove &&
-                gameField[currentLine][currentColumn + 3] == playerMove)
+            if (gameField[currentLine][currentColumn] == playerSymbol &&
+                gameField[currentLine][currentColumn + 1] == playerSymbol &&
+                gameField[currentLine][currentColumn + 2] == playerSymbol &&
+                gameField[currentLine][currentColumn + 3] == playerSymbol)
                 winPlayer = playerID;
             currentLine++;
             if (currentLine == 6) {
@@ -75,10 +74,10 @@ public class Game {
         currentLine = 0;
         currentColumn = 0;
         while (currentLine < 3) {
-            if (gameField[currentLine][currentColumn] == playerMove &&
-                gameField[currentLine+1][currentColumn] == playerMove &&
-                gameField[currentLine+2][currentColumn] == playerMove &&
-                gameField[currentLine+3][currentColumn] == playerMove)
+            if (gameField[currentLine][currentColumn] == playerSymbol &&
+                gameField[currentLine+1][currentColumn] == playerSymbol &&
+                gameField[currentLine+2][currentColumn] == playerSymbol &&
+                gameField[currentLine+3][currentColumn] == playerSymbol)
                 winPlayer = playerID;
             currentColumn++;
             if (currentColumn == 7) {
@@ -91,10 +90,10 @@ public class Game {
         currentLine = 0;
         currentColumn = 0;
         while (currentLine < 3) {
-            if (gameField[currentLine][currentColumn] == playerMove &&
-                gameField[currentLine+1][currentColumn+1] == playerMove &&
-                gameField[currentLine+2][currentColumn+2] == playerMove &&
-                gameField[currentLine+3][currentColumn+3] == playerMove)
+            if (gameField[currentLine][currentColumn] == playerSymbol &&
+                gameField[currentLine+1][currentColumn+1] == playerSymbol &&
+                gameField[currentLine+2][currentColumn+2] == playerSymbol &&
+                gameField[currentLine+3][currentColumn+3] == playerSymbol)
                 winPlayer = playerID;
             currentColumn++;
             if (currentColumn == 4) {
@@ -102,15 +101,15 @@ public class Game {
                 currentLine++;
             }
         }
-  
+
         // check the Field diagonal (< left)
         currentLine = 0;
         currentColumn = 6;
         while (currentLine < 3) {
-            if (gameField[currentLine][currentColumn] == playerMove &&
-                gameField[currentLine+1][currentColumn-1] == playerMove &&
-                gameField[currentLine+2][currentColumn-2] == playerMove &&
-                gameField[currentLine+3][currentColumn-3] == playerMove)
+            if (gameField[currentLine][currentColumn] == playerSymbol &&
+                gameField[currentLine+1][currentColumn-1] == playerSymbol &&
+                gameField[currentLine+2][currentColumn-2] == playerSymbol &&
+                gameField[currentLine+3][currentColumn-3] == playerSymbol)
                 winPlayer = playerID;
             currentColumn--;
             if (currentColumn == 2) {
@@ -124,5 +123,4 @@ public class Game {
 
         return winPlayer;
     }
-
 }
